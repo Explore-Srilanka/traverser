8a76d4dbdb181eb24785ba448bee354e5193444c
nadeera3784 <nadeera3784@gmail.com>
2021-02-23 10:25:03 +0530

    feat : flash message added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,16 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard') }} <a href="{{ route('add_district') }}" class="btn btn-sm btn-primary float-right">New +</a><div class="clearfix"></div></div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    @include('flash')
 
                     {{ __('You are logged in!') }}
                 </div>
